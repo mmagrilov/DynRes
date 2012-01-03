4d0ee4c1fe6eae162e48c7607e1a427222fed5ce
Michael Magrilov <mmagrilov@gmail.com>
2012-01-03 23:10:01 +0200

    Version 2.0 of DynResManager.lua (boundaries / screen size functions added)

--- DynResManager.lua
+++ DynResManager.lua
@@ -7,7 +7,13 @@ local public = {} 	-- This form of module uses only locals,
 --  
 -- written by Michael Magrilov, mmagrilov@gmail.com
 -- 08.09.2011
---            
+--         
+-- ================================================================================================
+-- Version 2.0
+-- Changes: 
+-- 1) New functions for getting screen boundaries and screen size
+-- 2) "base" forceLoadRes value added as a synonim to "low"
+
 -- ================================================================================================
 
 
@@ -20,14 +26,16 @@ local constHiResPicScale 	= 2.0	-- in this case Hi-res images must be twice as b
 local constHiResSuffix     	= "@2x"	-- Low: "pic.png"; High: "pic@2x.png"
 									-- a note: in this version there can be only 1 dot in image name (names like abc.def.jpg are not allowed)
 
-local constHiResThreshold  	= 1.8   -- Hi-res pic will be loaded if the app is "magnified" by this or greater value;
-									-- if width in config.lua is 320, then hi-res is loaded starting from device width = 320 * 1.8 = 576 
-									-- (I assume that scale in config.lua = "letterbox" or "zoomeven" and device proportions are "taller"
-									-- than those of the application)
-									
-									-- If you need more than two resolution levels, 
-									-- it's possible to turn constHiResPicScale, constHiResSuffix and constHiResThreshold into tables
-									-- (of course, this requires also changes in local functions)
+local constHiResThreshold  	= 1.49   -- Hi-res pic will be loaded if the app is "magnified" by this or greater value;
+						-- if width in config.lua is 320 and threshhold is 1.8, then hi-res is loaded starting from device width = 320 * 1.8 = 576 
+						-- if width in config.lua is 320 and threshhold is 1.5, then hi-res is loaded starting from device width = 320 * 1.5 = 480
+						-- (I assume that scale in config.lua = "letterbox" or "zoomeven" and device proportions are "taller"
+						-- than those of the application);
+						-- because of scale rounding error it's better to take 1.79 instead of 1.8 and 1.49 instead of 1.5
+						
+						-- If you need more than two resolution levels, 
+						-- it's possible to turn constHiResPicScale, constHiResSuffix and constHiResThreshold into tables
+						-- (of course, this requires also changes in local functions)
 
 
 									
@@ -48,13 +56,14 @@ end -- haveToLoadHiRes
 public.haveToLoadHiRes = haveToLoadHiRes
 
 ---------------------------------------------------------------
-local function loadImage(imageFileName, forceLoadRes)	-- The main method; optional parm forceLoadRes = "high" / "low" or empty
+local function loadImage(imageFileName, forceLoadRes)	-- The main method; optional parm forceLoadRes = "high" / "low" ("base") or empty
 ---------------------------------------------------------------
 local imageFileName  = tostring(imageFileName) or false
 if imageFileName == "" then return nil end
 
 local forceLoadRes = string.lower(tostring(forceLoadRes)) or false
-if forceLoadRes ~= "high" and forceLoadRes ~= "low" then forceLoadRes = "" end
+if forceLoadRes ~= "high" and forceLoadRes ~= "low" and forceLoadRes ~= "base" then forceLoadRes = "" end
+if forceLoadRes == "base" then forceLoadRes = "low" end
 
 	local hiResImageFileName
 	local tempImage = nil
@@ -126,7 +135,7 @@ public.getHiResThreshold = getHiResThreshold
 ------------------------------------------------------
 local function minVisibleY() 
 ------------------------------------------------------
-	return display.screenOriginY
+	return display.screenOriginY 
 end
 public.minVisibleY = minVisibleY
 
@@ -135,6 +144,7 @@ local function maxVisibleY(scaleMode, yAlign) -- default: "letterbox", "center"
 ------------------------------------------------------
 -- in "letterbox" or "zoomeven" scaling modes works only with yAlign = "bottom" or "center" (default); 
 -- doesn't work for yAlign = "top" (we can't get the height of the bottom margin)
+-- this is true also for screenHeight... functions
 
 local scaleMode = string.lower(tostring(scaleMode)) or ""
 if scaleMode ~= "none" and scaleMode ~= "zoomeven" and scaleMode ~= "zoomstretch" then scaleMode = "letterbox" end
@@ -173,6 +183,7 @@ local function maxVisibleX(scaleMode, xAlign) -- default: "letterbox", "center"
 ------------------------------------------------------
 -- in "letterbox" or "zoomeven" scaling modes works only with xAlign = "right" or "center" (default); 
 -- doesn't work for xAlign = "left" (we can't get the width of the right margin)
+-- this is true also for screenWidth... functions
 
 local scaleMode = string.lower(tostring(scaleMode)) or ""
 if scaleMode ~= "none" and scaleMode ~= "zoomeven" and scaleMode ~= "zoomstretch" then scaleMode = "letterbox" end
@@ -203,26 +214,9 @@ public.maxVisibleX = maxVisibleX
 ------------------------------------------------------
 local function screenHeightAppPix(scaleMode, yAlign) -- default: "letterbox", "center" 
 ------------------------------------------------------
--- screen height in "application pixels"
--- in "letterbox" scaling mode works only with yAlign = "bottom" or "center" (default); 
--- doesn't work for yAlign = "top" (we can't get the height of the bottom margin)
-
-local scaleMode = string.lower(tostring(scaleMode)) or ""
-if scaleMode ~= "none" and scaleMode ~= "zoomeven" and scaleMode ~= "zoomstretch" then scaleMode = "letterbox" end
-
-local yAlign = string.lower(tostring(yAlign)) or ""
-if yAlign ~= "bottom" then yAlign = "center" end
-
-	if scaleMode == "none" or scaleMode == "letterbox" then
-		if yAlign == "center" then
-			return display.viewableContentHeight - 2 * display.screenOriginY
-		else
-			return display.viewableContentHeight - display.screenOriginY
-		end
-	else
-		return display.viewableContentHeight
-	end
-
+	local scaleMode = string.lower(tostring(scaleMode)) or ""
+	if scaleMode ~= "none" and scaleMode ~= "zoomeven" and scaleMode ~= "zoomstretch" then scaleMode = "letterbox" end
+	return ( maxVisibleY(scaleMode, yAlign) - minVisibleY(scaleMode) )  -- not "+ 1" because "min" corresponds to 0
 end
 public.screenHeightAppPix = screenHeightAppPix
 
@@ -230,7 +224,8 @@ public.screenHeightAppPix = screenHeightAppPix
 local function screenHeightPhysPix(scaleMode, yAlign) -- default: "letterbox", "center" 
 ------------------------------------------------------
 -- screen height in "physical pixels"
--- I use "0.51" for rounding instead of 0.5 because screenOriginX & screenOriginY are rounded to a half pixel accuracy 
+-- I use "0.51" for rounding instead of 0.5 because screenOriginX & screenOriginY are rounded to a half pixel accuracy;
+-- still, the function can give one pixel error for "zoomeven" scaling mode 
 	return math.floor(screenHeightAppPix(scaleMode, yAlign) / display.contentScaleY + 0.51)
 end
 public.screenHeightPhysPix = screenHeightPhysPix
@@ -240,26 +235,9 @@ public.screenHeightPhysPix = screenHeightPhysPix
 ------------------------------------------------------
 local function screenWidthAppPix(scaleMode, xAlign) -- default: "letterbox", "center" 
 ------------------------------------------------------
--- screen width in "application pixels"
--- in "letterbox" scaling mode works only with xAlign = "right" or "center" (default); 
--- doesn't work for xAlign = "left" (we can't get the width of the right margin)
-
-local scaleMode = string.lower(tostring(scaleMode)) or ""
-if scaleMode ~= "none" and scaleMode ~= "zoomeven" and scaleMode ~= "zoomstretch" then scaleMode = "letterbox" end
-
-local xAlign = string.lower(tostring(xAlign)) or ""
-if xAlign ~= "right" then xAlign = "center" end
-
-	if scaleMode == "none" or scaleMode == "letterbox" then
-		if xAlign == "center" then
-			return display.viewableContentWidth - 2 * display.screenOriginX
-		else
-			return display.viewableContentWidth - display.screenOriginX
-		end
-	else
-		return display.viewableContentWidth
-	end
-
+	local scaleMode = string.lower(tostring(scaleMode)) or ""
+	if scaleMode ~= "none" and scaleMode ~= "zoomeven" and scaleMode ~= "zoomstretch" then scaleMode = "letterbox" end
+	return ( maxVisibleX(scaleMode, xAlign) - minVisibleX(scaleMode) )  -- not "+ 1" because "min" corresponds to 0
 end
 public.screenWidthAppPix = screenWidthAppPix
 
@@ -267,7 +245,8 @@ public.screenWidthAppPix = screenWidthAppPix
 local function screenWidthPhysPix(scaleMode, xAlign) -- default: "letterbox", "center" 
 ------------------------------------------------------
 -- screen width in "physical pixels"
--- I use "0.51" for rounding instead of 0.5 because screenOriginX & screenOriginY are rounded to a half pixel accuracy 
+-- I use "0.51" for rounding instead of 0.5 because screenOriginX & screenOriginY are rounded to a half pixel accuracy; 
+-- still, the function can give one pixel error for "zoomeven" scaling mode
 	return math.floor(screenWidthAppPix(scaleMode, xAlign) / display.contentScaleX + 0.51)
 end
 public.screenWidthPhysPix = screenWidthPhysPix
